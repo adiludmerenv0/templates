68fddd7231b5a664c568c4956a04903890995a1e
Adi Ludmer <adi.ludmer@env0.com>
2020-06-10 15:05:59 +0300

    add tf11

---
+++ aws/test_expression_tf11/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "ludmer-tf11-test-bucket"
+  acl    = "private"
+
+  tags = {
+    "Name"                    = "My bucket"
+    Unquoted1                 = "I wanna be quoted"
+    "AnotherName"             = "Yo"
+    Unquoted2                 = "I really wanna be quoted"
+    Unquoted3                 = "I really really wanna be quoted and I got a comma",
+    "${max(1, 2)}"            = "Test function"
+    "${local.localTagKey}"    = "Test expression"
+    "${local.localTagKey2}"   = "Test variable as key"
+    "Yo-${local.localTagKey}" = "Test variable inside key"
+    "Test variable as value" = "${local.localTagKey}"
+    "Test variable inside value"  = "Yo-${local.localTagKey}"
+  }
+}
+
+locals {
+  localTagKey = "localTagKey"
+  localTagKey2 = "localTagKey2"
+}
